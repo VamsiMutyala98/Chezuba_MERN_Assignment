294fe61a7b0540d6abe8d7d60f747d5e2456534c
VamsiMutyala98 <vamshi.mutyala@fissionlabs.com>
2023-09-21 10:52:31 +0530

    Server: Give item and branch get access to customers

--- Backend/routes/branch.js
+++ Backend/routes/branch.js
@@ -3,7 +3,7 @@ const { create, update, getAll, deleteItem } = require('../controller/branch.con
 const { authWithParams } = require('../utils/helper');
 const router = express.Router();
 
-router.route('/branch').get(authWithParams(['ADMIN']), getAll).post(authWithParams(['ADMIN']), create);
+router.route('/branch').get(authWithParams(['ADMIN', 'CUSTOMER']), getAll).post(authWithParams(['ADMIN']), create);
 router.route('/branch/:id').put(authWithParams(['ADMIN']), update).delete(authWithParams(['ADMIN']), deleteItem);
 
 module.exports = router;
--- Backend/routes/item.js
+++ Backend/routes/item.js
@@ -3,7 +3,7 @@ const { create, update, getAll, deleteItem } = require('../controller/item.contr
 const { authWithParams } = require('../utils/helper');
 const router = express.Router();
 
-router.route('/items').get(authWithParams(['ADMIN']), getAll).post(authWithParams(['ADMIN']), create);
+router.route('/items').get(authWithParams(['ADMIN', 'CUSTOMER']), getAll).post(authWithParams(['ADMIN']), create);
 router.route('/item/:id').put(authWithParams(['ADMIN']), update).delete(authWithParams(['ADMIN']), deleteItem);
 
 module.exports = router;
